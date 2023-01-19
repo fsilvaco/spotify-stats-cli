52bd780fb9218211e4db9e39db1e10af7be4f025
Flavio Silva <flavio.silva@g.globo>
2023-01-19 16:08:24 -0300

    Added request to GET user data

--- cmd/login.go
+++ cmd/login.go
@@ -11,7 +11,7 @@ import (
 const (
 	PORT             string = ":8080"
 	CLIENT_ID        string = "33cfd3aa25f144a18bc39ba4f7b6302c"
-	SCOPE            string = "user-top-read"
+	SCOPE            string = "user-top-read user-read-private user-read-email"
 	REDIRECT_URI     string = "http://localhost" + PORT + "/auth"
 	SPOTIFY_AUTH_URL string = "https://accounts.spotify.com/authorize?" + "client_id=" + CLIENT_ID + "&response_type=token&scope=" + SCOPE + "&redirect_uri=" + REDIRECT_URI
 )
--- spotify/api.go
+++ spotify/api.go
@@ -1,7 +1,9 @@
 package spotify
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -13,7 +15,53 @@ type Server struct {
 	SpotifyAuthURL string
 }
 
+type User struct {
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+}
+
 var token string
+
+func (s Server) getCurrentUser(token string) {
+
+	url := "https://api.spotify.com/v1/me"
+	method := "GET"
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var user User
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Hi, " + user.DisplayName + "\nChoose which information you want to see:")
+
+}
 
 func (s Server) responseAuthStaticFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/auth.html")
@@ -25,6 +73,8 @@ func (s Server) getTokenUser(w http.ResponseWriter, r *http.Request) {
 	if token != "" {
 		fmt.Println("Login successful!")
 	}
+
+	s.getCurrentUser(token)
 
 }
 
